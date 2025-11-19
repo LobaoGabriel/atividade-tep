f38b1d5e5be52a92aab319a17d1780032f287213
Gabriel <gabriel.lobao701@gmail.com>
2025-11-18 21:16:37 -0300

    primeiro commit

---
+++ p_commit.py
@@ -0,0 +1,11 @@
+import time
+
+print('--- INICIANDO PROGRAMA ---')
+
+amigos = ['Luci Fontis', 'Galego Fornite', 'Ynara Roballo', 'Lobas', 'Jayne','Mendoncinha']
+
+for x in amigos:
+    print(f'{x} é um querido!')
+    time.sleep(2)
+
+print('--- FIM DO PROGRAMA ---')
